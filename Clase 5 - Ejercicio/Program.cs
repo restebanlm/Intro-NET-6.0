a5de4e6ef4f02e7d4b2ef3055b26bd71306c6b3e
Esteban <49765124+restebanlm@users.noreply.github.com>
2022-05-18 01:57:34 -0300

    Add files via upload

---
+++ Clase 5 - Ejercicio/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.WriteLine("Ingrese su nombre: ");
+string nombre = Console.ReadLine();
+Console.WriteLine($"¡Hola {nombre}!");
+Console.WriteLine("¿Desea continuar con la ejecución? [S=SI / N=NO]");
+string accion = Console.ReadLine();
+
+while (accion.ToUpper() == "S")
+{
+    Console.Clear();
+    Console.WriteLine("Ingrese su nombre: ");
+    nombre = Console.ReadLine();
+    Console.WriteLine($"¡Hola {nombre}!");
+    Console.WriteLine("¿Desea continuar? [S=SI / N=NO]");
+    accion = Console.ReadLine();
+}
+if (accion.ToUpper() == "N")
+    Console.WriteLine("Programa finalizado correctamente");
+else
+    Console.WriteLine("Opción no válida");
